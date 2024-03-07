cf0cb5abcb4238ae68cda001179b175ecdc9104d
Ivan Bolotov <144904919+IvanJaba@users.noreply.github.com>
2024-03-07 20:09:43 +0500

    Delete main_var_1_2.kt

--- main_var_1_2.kt
+++
@@ -1,6 +0,0 @@
-fun drawCircleWithFor(radius: Int) {
-    for (x in 0..(2 * radius)) {
-        val y = Math.sqrt((radius * radius - (x - radius) * (x - radius)).toDouble()).toInt()
-        println(" ".repeat(radius - y) + "*".repeat(2 * y))
-    }
-}
